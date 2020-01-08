be421e781f6e43f679ef26ee32d279ea4ac1a3e3
porkostomus <porkostomus@mail.com>
2020-01-08 03:44:34 -0800

    better tests

--- src/test/trig/triangle-test.cljs
+++ src/test/trig/triangle-test.cljs
@@ -7,17 +7,31 @@
   [n x]
   (/ (.round js/Math (* x n)) x))
 
-(deftest solves-missing-angle
-  (is (= 35 (:angle3 (tri/solve-triangle {:angle3 nil
-                                          :angle1 40
+(deftest two-angles-test
+  (is (= 35 (:angle3 (tri/solve-triangle {:angle1 40
                                           :angle2 105})))))
 
 (deftest one-side-all-angles-test
   (is (= 5.27 (round (:line2 (tri/solve-triangle {:angle3 35
                                                   :angle1 40
                                                   :angle2 105
-                                                  :line1  4.7})) 100))))
+                                                  :line1  4.7})) 100)))
+  (is (= 36 (round (:line2 (tri/solve-triangle {:angle3 109
+                                                :angle1 47
+                                                :angle2 24
+                                                :line1  46})) 1))))
+
+(deftest two-sides-one-angle-test
+  (is (= 184 (round (:line1 (tri/solve-triangle {:angle3 3
+                                                 :line2  915
+                                                 :line3  736})) 1)))
+  (is (= 262 (round (:line2 (tri/solve-triangle {:line3  250
+                                                 :angle1 56
+                                                 :line1  300})) 1))))
+
+
 
 (comment
+  (- 180 58)
   (:angle3 @tri/triangle)
  (run-tests) )
