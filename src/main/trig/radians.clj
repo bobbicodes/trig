9dadbd662306a6cf082120151be8aa16f818e30d
porkostomus <porkostomus@mail.com>
2021-05-27 21:14:03 -0600

    radians and degrees

---
+++ src/main/trig/radians.clj
@@ -0,0 +1,28 @@
+(ns trig.radians
+  "https://www.khanacademy.org/math/trigonometry/unit-circle-trig-func/intro-to-radians-trig/v/introduction-to-radians")
+
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn degrees 
+  "Converts radians to degrees. 
+   Numerator is considered to be a factor of pi"
+  [n d]
+  (* (/ 180 d) n))
+
+(defn radians
+  "Converts degrees to radians. 
+   Outputs a vector of the numerator (factor of pi) and denominator."
+  [deg]
+  [(/ deg (gcd deg 180))
+   (/ 180 (gcd deg 180))])
+
+(comment
+  (gcd 310 180)
+  (degrees 29 15)
+  (degrees 257 360.0)
+  (degrees 5 12)
+  (radians 310)
+  (radians 180))
