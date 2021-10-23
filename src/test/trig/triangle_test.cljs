b70b131710d34405bb917ac24ecdd31cc436057a
porkostomus <porkostomus@mail.com>
2021-10-23 11:18:36 -0700

    test trig ratios

--- src/test/trig/triangle_test.cljs
+++ src/test/trig/triangle_test.cljs
@@ -9,6 +9,8 @@
 
 (def pi js/Math.PI)
 
+(defn rad [deg] (* deg (/ pi 180)))
+
 (deftest infer-angle-test
   (is (= (tri/infer-angle-rad
           {:vertices ["R" "T" "S"]
@@ -36,9 +38,21 @@
           :lines [3 3 4.242640687119285]
           :angles [(/ pi 4) (/ pi 2) (/ pi 4)]})))
 
-#_(deftest two-angles-test
-  (is (= 35 (:angle3 (tri/solve-triangle {:angle1 40
-                                          :angle2 105})))))
+(deftest trig-ratios-test
+  (is (= (tri/trig-ratios {:lines [12 35 37]})
+         {:sin {:angle1 [35 37], :angle3 [12 37]}, 
+          :csc {:angle1 [37 35], :angle3 [37 12]}, 
+          :cos {:angle1 [12 37], :angle3 [35 37]}, 
+          :sec {:angle1 [37 12], :angle3 [37 35]}, 
+          :tan {:angle1 [35 12], :angle3 [12 35]}, 
+          :cot {:angle1 [12 35], :angle3 [35 12]}})))
+
+(deftest two-angles-test
+  (is (= {:lines [5 2.331538290774993 5.516889594812459]
+          :angles [0.4363323129985824 1.5707963267948966 1.1344640137963142]}
+         (tri/solve-triangle
+          {:lines [5 nil nil]
+           :angles [(rad 25) (/ pi 2) nil]}))))
 
 #_(deftest one-side-all-angles-test
   (is (= 5.27 (round (:line2 (tri/solve-triangle {:angle3 35
@@ -59,5 +73,5 @@
                                                  :line1  300})) 1))))
 
 (comment
- (run-tests) 
+ (run-tests)
   )
